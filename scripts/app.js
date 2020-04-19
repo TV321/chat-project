e50e1f8bae0d7e24b7123df9861f343ac591e24c
TV321 <tvlahovic7@gmail.com>
2020-04-20 01:36:09 +0200

    send chat

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,18 @@
 const chatList = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat')
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = newChatForm.message.value.trim()
+    chat.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(console.log)
+})
 
 const chat = new Chatroom('jack', 'general')
 const chatUI = new ChatUI(chatList)
 
 
 chat.getChats(data => chatUI.render(data))
+
+
